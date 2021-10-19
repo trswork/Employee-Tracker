73734827aad642e4df497e0bd6f57415c1148104
trswork <87744214+trswork@users.noreply.github.com>
2021-10-18 21:53:36 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees
+(first_name, last_name, title, department, salary, manager)
+VALUES
+('John', 'Doe', 'Sales Lead', 'Sales', '100000', 'Ashley Rodriguez')
+('Mike', 'Chan', 'Salesperson', 'Sales', '80000', 'John Doe')
+('Ashley', 'Rodriguez', 'Lead Engineer', 'Engineer', '150000', 'null')
+('Kevin', 'Tupik', 'Software Engineer', 'Engineer', '120000', 'Ashley Rodriguez')
+('Malia', 'Brown', 'Accountant', 'Finance', '125000', 'null')
+('Sarah', 'Lourd', 'Legal Team Lead', 'Legal', '250000', 'null')
+('Tom', 'Allen', 'Lawyer', 'Legal', '190000', 'Sarah Lourd')
+('Tammer', 'Galal', 'Software Engineer', 'Engineer', '120000', 'Kevin Tupik')
